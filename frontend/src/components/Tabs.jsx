fba1eb001fd5896dd32700611a2296e1f6c064e5
hamzahashmi033 <hashmihamza846@gmail.com>
2024-09-26 02:24:42 +0500

    Add Status and stage in backend projects enitity

--- frontend/src/components/Tabs.jsx
+++ frontend/src/components/Tabs.jsx
@@ -14,7 +14,7 @@ const TabsComp = () => {
                 <Tab _selected={{ color: '#334876', bg: '#DBE2F0', fontWeight: "700" }} className="text-gray-500 w-[50%]">Invited</Tab>
             </TabList>
             <TabPanels>
-                <TabPanel className="mt-4 rounded-md" bg={"#EBF0FA"}>
+                <TabPanel className="mt-4 rounded-md  " bg={"#EBF0FA"}>
                     <div className={`flex cursor-pointer space-x-2 items-center p-2 ${item == "Dashboard" ? "bg-white font-semibold" : ""}  rounded-md mb-2 hover:font-semibold transition duration-300 ease-in-out`} onClick={() => setItem("Dashboard")}>
                         {
                             item == "Dashboard" ? <UisStar size={20} color={"black"} /> : <Unicons.UilStar size={20} color={"gray"} />
@@ -44,8 +44,35 @@ const TabsComp = () => {
                         <AccordianComp />
                     </div>
                 </TabPanel>
-                <TabPanel>
-                    <p className="text-black">two!</p>
+                <TabPanel className="mt-4 rounded-md  " bg={"#EBF0FA"}>
+                    <div className={`flex cursor-pointer space-x-2 items-center p-2 ${item == "Dashboard" ? "bg-white font-semibold" : ""}  rounded-md mb-2 hover:font-semibold transition duration-300 ease-in-out`} onClick={() => setItem("Dashboard")}>
+                        {
+                            item == "Dashboard" ? <UisStar size={20} color={"black"} /> : <Unicons.UilStar size={20} color={"gray"} />
+                        }
+                        <h1 className="text-sky-950 text-md ">Invited Projects</h1>
+                    </div>
+                    <div className={`flex cursor-pointer space-x-2 items-center p-2 ${item == "Project" ? "bg-white font-semibold" : ""}  rounded-md mb-2 hover:font-semibold transition duration-300 ease-in-out`} onClick={() => setItem("Project")}>
+                        {
+                            item == "Project" ? <UisStar size={20} color={"black"} /> : <Unicons.UilStar size={20} color={"gray"} />
+                        }
+                        <h1 className="text-sky-950 text-md ">Linked People</h1>
+                    </div>
+                    <div className={`flex cursor-pointer space-x-2 items-center p-2 ${item == "Client" ? "bg-white font-semibold" : ""}  rounded-md mb-2 hover:font-semibold transition duration-300 ease-in-out`} onClick={() => setItem("Client")}>
+                        {
+                            item == "Client" ? <UisStar size={20} color={"black"} /> : <Unicons.UilStar size={20} color={"gray"} />
+                        }
+                        <h1 className="text-sky-950 text-md ">Team Management</h1>
+                    </div>
+                    <div className={`flex cursor-pointer space-x-2 items-center p-2 ${item == "Emails" ? "bg-white font-semibold" : ""}  rounded-md mb-2 hover:font-semibold transition duration-300 ease-in-out`} onClick={() => setItem("Emails")}>
+                        {
+                            item == "Emails" ? <UisStar size={20} color={"black"} /> : <Unicons.UilStar size={20} color={"gray"} />
+                        }
+                        <h1 className="text-sky-950 text-md ">Alerts</h1>
+                    </div>
+                    <div className="border border-gray-300 mt-4 w-full"></div>
+                    <div className="mt-4">
+                        <AccordianComp />
+                    </div>
                 </TabPanel>
             </TabPanels>
         </Tabs>
